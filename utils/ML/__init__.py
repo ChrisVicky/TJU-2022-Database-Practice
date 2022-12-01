5b2a2221afb7f78cb76b81032b65c0b86bb3d766
lcf <kevinfinley@163.com>
2022-12-01 14:48:11 +0800

    added cache search

--- utils/ML/__init__.py
+++ utils/ML/__init__.py
@@ -9,3 +9,17 @@ def get_emb(sentence_list:list[str]):
     return embeddings.astype(np.float64)
     # return np.array(embeddings)
 
+def cos_sim(a: np.ndarray, b: np.ndarray):
+    """
+    Computes the cosine similarity cos_sim(a[i], b[j]) for all i and j.
+    :return: Similarity matrix with res[i][j]  = cos_sim(a[i], b[j])
+    """
+    if len(a.shape) == 1:
+        a = a[np.newaxis, :]
+
+    if len(b.shape) == 1:
+        b = b[np.newaxis, :]
+
+    a_norm = np.linalg.norm(a, axis=1, keepdims=True)
+    b_norm = np.linalg.norm(b, axis=1, keepdims=True)
+    return np.dot(a / a_norm, (b / b_norm).T)
